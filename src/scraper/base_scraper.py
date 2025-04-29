4031ac33e4fba207fa4a89a86a165ef13a151ff2
Millia <learna2103@gmail.com>
2025-04-29 13:32:27 +0800

    Adding in base_Scraper

--- src/scraper/base_scraper.py
+++ src/scraper/base_scraper.py
@@ -1,9 +1,9 @@
 import aiohttp
 from bs4 import BeautifulSoup
-from typing import Dict, List
-from dataclasses import field    
+from ..utils.logger import Project_Logger
+from dataclasses import field, dataclass    
 from .proxy import proxy_list, ProxyManager
-from ..utils.logger import logging
+from typing import Dict, List, Any, Callable, Optional, Union,TypeVar
 
 
 # TODO: Rapihkan
@@ -12,23 +12,43 @@ from ..utils.logger import logging
 # TODO: Jadikan class (done)
 
 
+@dataclass
+class ScraperResponese:
+   status: int
+   content: Any
+   url: str
+   headers: Dict[str, str] = field(default_factory=dict)
+   error: Optional[str] = None
+
+
 class BaseScraper:
 
-   def __init__(self):
-      self.proxy_manager = ProxyManager(proxy_list)
+   def __init__(self, use_proxies: bool = True, logger_name: str = __name__):
+      self.proxy_manager = ProxyManager(proxy_list) if use_proxies else None
+      self.logger = Project_Logger(logger_name)
+      self.default_header = {
+         'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'
+         }
 
-   def header(self):
-      return {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36'}
+   def get_header(self, additional_headers: Optional[Dict[str, str]] = None) -> Dict[str, str]:
+      if additional_headers:
+         return {**self.default_header, **additional_headers}
+      return self.additional_headers
 
+   async def process_json(self, response: aiohttp.ClientResponse) -> Dict[str, Any]:
+        """Process response as JSON"""
+        return await response.json()
 
-   def json_(self,response: str) -> None:
-      return response.json()
+   async def process_text(self, response: aiohttp.ClientResponse) -> str:
+        """Process response as JSON"""
+        return await response.text()
 
-   def text_(self, response: dict) -> None:
-      return response.text()
+   async def process_soup(self, response: aiohttp.ClientResponse) -> BeautifulSoup:
+        """Process response as JSON"""
+        text = await response.text()
+        return BeautifulSoup(text, 'html5lib')
 
-   def soup(self, soup: str) -> None:
-      return BeautifulSoup(soup, 'html5lib')
+   async
 
    async def scrape_info_(self, content: str = None, element: str = None) -> None:
       soup = BeautifulSoup(content, 'html5lib')
